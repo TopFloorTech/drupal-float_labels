ee1cf36b40c0c56a6f9c7eb0d604287eafa1c984
Ben McClure <ben.mcclure@gmail.com>
2016-12-25 12:12:05 -0600

    Refactor JS and make CSS more generic and responsive by using ems.

--- js/float_labels.js
+++ js/float_labels.js
@@ -1,27 +1,55 @@
 (function ($) {
   'use strict';
 
-  function processFloatLabels(context) {
-    $('.float-labels-include', context).not('.float-labels-processed').each(function () {
-      var $element = $(this);
-      var id = $(this).attr('id');
-      var $label = $('label[for=' + id + ']');
-      var labelText = $label.text();
-      var required = $element.attr('required');
+  function getFloatLabelElements(context, settings) {
+    var $forms = $('.float-labels-include-children', context);
+
+    var $elements = $('.float-labels-include', context)
+      .not('.float-labels-processed')
+      .add($forms.find(':input').not('.float-labels-processed, :button, :hidden'));
+
+    if (settings.includes && settings.includes.length > 0) {
+      $elements = $elements.is(settings.includes.join(', '));
+    }
+
+    if (settings.excludes && settings.excludes.length > 0) {
+      $elements = $elements.not(settings.excludes.join(', '));
+    }
+
+    return $elements;
+  }
+
+  function getLabel(element) {
+    var id = element.attr('id');
+    var $label = $('label[for=' + id + ']');
+    var text = '';
+
+    if ($label.length > 0) {
+      text = $label.text();
 
       $label.remove();
+    }
+    else if (element.hasAttribute('title')) {
+      text = element.attr('title');
+    }
+    else if (element.hasAttribute('placeholder')) {
+      text = element.attr('placeholder');
+    }
 
-      var $wrapper = $('<div class="float-labels-wrapper">').css({'font-size': $element.css('font-size')});
+    return text;
+  }
 
-      if (required) {
-        $wrapper.addClass('float-labels-required');
-      }
+  function processFloatLabels($elements) {
+    $elements.each(function () {
+      var $element = $(this).addClass('float-labels-item');
+      var id = $(this).attr('id');
+      var labelText = getLabel($element);
+      var required = $element.attr('required');
 
-      $element.wrap($wrapper);
-
-      $('<label class="float-labels-label">').attr('for', id).text(labelText).insertBefore($element);
-
-      $element.addClass('float-labels-processed');
+      $element
+        .wrap($('<div class="float-labels-wrapper">').toggleClass('float-labels-required', required))
+        .before($('<label class="float-labels-label">').attr('for', id).text(labelText))
+        .addClass('float-labels-processed');
     });
 
     $('.float-labels-processed').on('focus blur', function (e) {
@@ -31,7 +59,15 @@
 
   Drupal.behaviors.floatLabels = {
     attach: function (context, settings) {
-      processFloatLabels(context);
+      if (settings.hasOwnProperty('float_labels')) {
+        settings = settings.float_labels;
+      } else {
+        settings = Drupal.settings.float_labels;
+      }
+
+      var $elements = getFloatLabelElements(context, settings);
+
+      processFloatLabels($elements);
     }
   };
 }(jQuery));
